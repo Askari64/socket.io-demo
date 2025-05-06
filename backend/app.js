23d1d8cd15c39ffad72006003e285fbf0128223d
Askari <askarir64@gmail.com>
2025-05-06 10:07:18 +0530

    added cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,13 @@ const port = 3000;
 const app = express();
 const httpServer = createServer(app)
 
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials:false
+  }
+})
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
