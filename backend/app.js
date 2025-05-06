e483c8a415a4a26bb3227c63627b0c6345603551
Askari <askarir64@gmail.com>
2025-05-06 12:14:43 +0530

    Emit and Broadcast implementation

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,36 @@
 import express from "express";
 import { Server } from "socket.io";
-import {createServer} from 'http'
+import { createServer } from "http";
 
 const port = 3000;
 const app = express();
-const httpServer = createServer(app)
+const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
-    credentials:false
-  }
-})
+    credentials: false,
+  },
+});
 
-app.get('/', (req, res) =>{
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-io.on("connection", (socket)=> {
-    console.log('User Connected')
-    console.log("Id", socket.id)
-})
+io.on("connection", (socket) => {
+  console.log("Connected Id", socket.id);
+
+  //sends message/event to current client only
+  socket.emit("Welcome", "Welcome to the server ");
+
+  //this emits message/event to other client instance instead of the client instance that refreshes/calls
+  socket.broadcast.emit("broadcastwelcome", `${socket.id} has joined`);
+
+  socket.on('disconnect', ()=>{
+    console.log(`User Disconnected ${socket.id}`)
+  })
+});
 
 httpServer.listen(3000, () => {
   console.log(`Server started on ${port}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import {io} from 'socket.io-client'
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 
 function App() {
   const socket = io("http://localhost:3000");
-  return (
-    <div>App</div>
-  )
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected", socket.id);
+    });
+
+    socket.on("Welcome", (s) => {
+      console.log(s);
+    });
+
+    socket.on("broadcastwelcome", (s) => {
+      console.log(s);
+    });
+
+    return ()=>{
+      socket.disconnect()
+    }
+  });
+
+  return <div>App</div>;
 }
 
-export default App
+export default App;
